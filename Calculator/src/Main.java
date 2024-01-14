465009837611b9531819ea2b737cd8113f827ccc
StarkowW <109904012+MegaStark@users.noreply.github.com>
2024-01-14 20:21:25 +0100

    Add files via upload

---
+++ Calculator/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+
+            int text1 = (50);
+            int text2 = (10);
+
+            System.out.println(text1 + text2);
+            System.out.println(text1 - text2);
+            System.out.println(text1 * text2);
+            System.out.println(text1 / text2);
+
+        int text3 = (20);
+        int text4 = (5);
+
+        System.out.println(text3 + text4);
+        System.out.println(text3 - text4);
+        System.out.println(text3 * text4);
+        System.out.println(text3 / text4);
+
+
+
+
+    }
+}
---
+++ Homework 10.01.2024/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int Zahl1 = (1);
+        byte Zahl2 = (2);
+        short Zahl3 = (3);
+        long Zahl4 = (444);
+        char Wort5 = 'W';
+        String say = "Hello !";
+        Double Zahl7 = (55.5);
+
+
+        System.out.println(Zahl1);
+        System.out.println(Zahl2);
+        System.out.println(Zahl3);
+        System.out.println(Zahl4);
+        System.out.println(Wort5);
+        System.out.println(say);
+        System.out.println(Zahl7);
+    }
+}
